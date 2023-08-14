f14cf1cac8471d819f978d913029164a75bdb5f6
DanielDanielsson <danielssondaniel94@gmail.com>
2023-08-14 07:27:31 +0200

    feat: wip ast experiments

--- packages/some-nextjs-code-to-validate/src/app/MyComponent.tsx
+++ packages/some-nextjs-code-to-validate/src/app/MyComponent.tsx
@@ -1,11 +1,33 @@
 /* eslint-disable no-console */
+
 import React from 'react';
+import { parse } from '@typescript-eslint/typescript-estree';
+
+const code = `const myFunction = ({a: string}) => {return null;}`;
+const ast = parse(code, {
+  loc: true,
+  range: true,
+});
+
+console.log('ast', ast);
+console.log('__________________');
+
+// const component = `export const MyComponent = ({ b, a, c, d }: MyComponentProps) => {
+//   return <div>MyComponent</div>;
+// };
+// `;
+// const astcomponent = parse(component, {
+//   // loc: true,
+//   // range: true,
+// });
+
+// console.log('astcomponent', astcomponent);
 
 export interface MyComponentProps {
-  a: string;
-  b: string;
-  c: string;
-  d: string;
+  a?: string;
+  b?: string;
+  c?: string;
+  d?: string;
 }
 
 export type MyType = {
@@ -27,5 +49,7 @@ export const MyComponent = ({ b, a, c, d }: MyComponentProps) => {
   console.log('b', b);
   console.log('c', c);
   console.log('d', d);
+  // console.log('astcomponent', astcomponent);
+
   return <div>MyComponent</div>;
 };
--- packages/some-nextjs-code-to-validate/src/app/page.tsx
+++ packages/some-nextjs-code-to-validate/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { MyComponent } from './MyComponent';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello world
+        <MyComponent />
       </div>
     </main>
   );
